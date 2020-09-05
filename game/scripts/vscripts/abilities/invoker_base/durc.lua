e5d26d6bccac7a6931a8887490791731c056ddbb
meskion <manufdh@hotmail.com>
2020-09-05 17:01:37 +0200

    added durc particles

--- game/scripts/vscripts/abilities/invoker_base/durc.lua
+++ game/scripts/vscripts/abilities/invoker_base/durc.lua
@@ -1,5 +1,5 @@
 require('abilities/invoker_base/invoker_base')
 
 function invoker_durc_datadriven_on_spell_start(keys)
-	invoker_replace_orb(keys, "particles/units/heroes/hero_invoker/invoker_exort_orb.vpcf")
+	invoker_replace_orb(keys, "particles/invoker_durc_orb.vpcf")
 end
